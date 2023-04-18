36d912f5270eaafe1fc4545b0ae187fe5c155187
breaklunch <elias.ibanez@tecsup.edu.pe>
2023-04-18 12:27:44 -0500

    version 2 commit

--- listar.php
+++ listar.php
@@ -1,28 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Lista</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-        
-    </head>
-    <body>
-    <h5>Listado</h5>
-          <hr/>
-          <table class="table">
-            <thead>
-              <tr>
-                echo $doc
-                <th>$doc</th>
-                <th>Nombres</th>
-                <th>Email</th>
-                <th>Edad</th>
-                <th>Opciones</th>
-              </tr>
-            </thead>
-            <tbody id="tbody"></tbody>
-          </table>
-    </body>
-</html>
+<?php
+include("conexion.php");
+$con = conexion();
+
+$doc = $_GET["doc"];
+$nom = $_GET["nom"];
+$ape = $_GET["ape"];
+$dir = $_GET["dir"];
+$cel = $_GET["cel"];
+
+
+echo "Documento: ", $doc;
+echo "Nombre: ", $nom;
+echo "Apellido", $ape;
+echo "Direccion", $dir;
+echo "Celular", $cel;
+?>
